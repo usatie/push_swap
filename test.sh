6d9eb0e56b511d562d76e413023f5d211fb1631f
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-05-31 13:14:18 +0900

    :sparkles: Add nafuka11 tester

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ARG="4 67 3 87 23"; ./push_swap $ARG | ./checker_Mac $ARG
+ARG="4 67 3 87 23"; ./push_swap $ARG | ./checker $ARG
+
+[ ! -d push_swap_tester ] && git clone https://github.com/nafuka11/push_swap_tester || echo "push_swap_tester is already cloned."
+cd push_swap_tester
+python3 push_swap_tester.py -l 100
