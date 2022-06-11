9eba2b2f5c9232a46a003b23ac9af321a6749e35
Shun Usami <susami@c1r13s4.42tokyo.jp>
2022-06-11 17:06:25 +0900

    :recycle: Refactor total_chunks() and chunk_contains()

--- src/sort/custom_sort2.c
+++ src/sort/custom_sort2.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 15:05:21 by susami            #+#    #+#             */
-/*   Updated: 2022/06/11 15:49:36 by susami           ###   ########.fr       */
+/*   Updated: 2022/06/11 17:05:57 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,37 @@
 #include "ft_printf.h"
 #define SEL_THRESHOLD 30
 
-static BOOL	is_contained(size_t e, size_t chunk, size_t n_chunk, t_ctx *c)
+static size_t	total_chunks(t_ctx *c)
+{
+	return (len_p(c) / SEL_THRESHOLD / 2 * 2 + 1);
+}
+
+static BOOL	chunk_contains(size_t e, size_t chunk, t_ctx *c)
 {
 	size_t	lower_border;
 	size_t	upper_border;
 
-	lower_border = len_p(c) * chunk / n_chunk;
-	upper_border = len_p(c) * (chunk + 1) / n_chunk - 1;
+	lower_border = len_p(c) * chunk / total_chunks(c);
+	upper_border = len_p(c) * (chunk + 1) / total_chunks(c) - 1;
 	if (lower_border <= e && e <= upper_border)
 		return (TRUE);
 	else
 		return (FALSE);
 }
 
-static void	partition_a2b(t_ctx *c,
-	size_t pb_chunk, size_t pbrb_chunk, size_t n)
+static void	create_chunk(t_ctx *c,
+	size_t pb_chunk, size_t pbrb_chunk)
 {
 	size_t	i;
 
-	ft_debug_printf("[partition_a2b(%d, %d, %d)] start\n",
-		pb_chunk, pbrb_chunk, n);
+	ft_debug_printf("[partition_a2b(%d, %d)] start\n",
+		pb_chunk, pbrb_chunk);
 	i = 0;
 	while (i < len_p(c))
 	{
-		if (is_contained(top_a(c), pb_chunk, n, c))
+		if (chunk_contains(top_a(c), pb_chunk, c))
 			pb(c);
-		else if (is_contained(top_a(c), pbrb_chunk, n, c))
+		else if (chunk_contains(top_a(c), pbrb_chunk, c))
 		{
 			pb(c);
 			rb(c);
@@ -48,27 +53,24 @@ static void	partition_a2b(t_ctx *c,
 			ra(c);
 		i++;
 	}
-	ft_debug_printf("[partition_a2b(%d, %d, %d)] end\n",
-		pb_chunk, pbrb_chunk, n);
+	ft_debug_printf("[partition_a2b(%d, %d)] end\n",
+		pb_chunk, pbrb_chunk);
 }
 
-static size_t	partition(t_ctx *c)
+static void	partition(t_ctx *c)
 {
 	size_t	i;
-	size_t	n;
 
-	n = len_p(c) / SEL_THRESHOLD / 2 * 2 + 1;
-	ft_debug_printf("[partitoin] start\n", n);
-	ft_debug_printf("[partitoin n = %d]\n", n);
+	ft_debug_printf("[partitoin] start\n");
 	i = 0;
-	while (i < n / 2)
+	while (i < total_chunks(c) / 2)
 	{
-		partition_a2b(c, n / 2 - i, n / 2 + 1 + i, n);
+		create_chunk(c, total_chunks(c) / 2 - i,
+			total_chunks(c) / 2 + 1 + i);
 		debug_print_ctx(c);
 		i++;
 	}
 	ft_debug_printf("[partition] end\n");
-	return (n);
 }
 
 static void	selection(t_ctx *c, size_t low, size_t high)
@@ -99,11 +101,9 @@ static void	selection(t_ctx *c, size_t low, size_t high)
 
 void	custom_sort2(t_ctx *c, size_t low, size_t high)
 {
-	size_t	pi;
-
 	if (high > low)
 	{
-		pi = partition(c);
+		partition(c);
 	}
 	if (low == 0 && high + 1 == len_p(c))
 	{
